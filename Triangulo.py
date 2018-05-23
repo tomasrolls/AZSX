44074b71867223e6f94fc64b8d943814daaa7b69
tomasrolls <39561566+tomasrolls@users.noreply.github.com>
2018-05-23 10:40:19 -0400

    Add files via upload

---
+++ Triangulo.py
@@ -0,0 +1,21 @@
+#Dados los lados de un triangulo, determinar su area, perimetro, tipo y grafico
+a = int(input("Ingrese primer lado: "))
+b = int(input("Ingrese segundo lado: "))
+c = int(input("Ingrese tercer lado: "))
+if (a+b)>c and (a+c)>b and (b+c)>a:
+    p = a + b + c
+    s = p/2
+    area = (s*(s-a)*(s-b)*(s-c))**.5
+    if a != b and b != c and a != c :
+        print("Triangulo Escaleno de perimetro =",p," y area =",area)
+        #Llamar graficador
+    else :
+        if (a == b) and (b == c) :
+            print("Triangulo Equilatero de perimetro =",p," y area =",area)
+        else :
+            print("Triangulo Isósceles de perimetro =",p," y area =",area)
+else :
+    print("No es triangulo")
+            
+            
+        
